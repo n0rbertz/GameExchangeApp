ce0ff988383806c2ecf1e06201b143ef53db1d15
n0rbertz <norbz123@gmail.com>
2022-01-26 16:45:39 +0100

    make ICollections virtual in model classes

--- GameExchangeApp/Models/Game.cs
+++ GameExchangeApp/Models/Game.cs
@@ -11,7 +11,7 @@ namespace GameExchangeApp.Models
         [Key]
         public int Id { get; set; }
         public string Title { get; set; }
-        public ICollection<Gamer> Gamers { get; set; }
+        public virtual ICollection<Gamer> Gamers { get; set; }
 
         public Game()
         {
