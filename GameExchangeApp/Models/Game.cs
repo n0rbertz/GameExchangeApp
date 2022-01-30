446621f65b2fff6af4e08b2c364e282f1101fb1c
n0rbertz <norbz123@gmail.com>
2022-01-30 01:21:17 +0100

    add JsonIgnore to navigation property on Game model

--- GameExchangeApp/Models/Game.cs
+++ GameExchangeApp/Models/Game.cs
@@ -2,7 +2,9 @@
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace GameExchangeApp.Models
 {
@@ -11,6 +13,7 @@ namespace GameExchangeApp.Models
         [Key]
         public int Id { get; set; }
         public string Title { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Gamer> Gamers { get; set; }
 
         public Game()
