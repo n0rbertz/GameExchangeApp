02c7f633b178f0a2e08c37c46873561872fc24ae
n0rbertz <norbz123@gmail.com>
2022-03-03 00:03:40 +0100

    create button to show owned games of a user, create game card component

---
+++ GameExchangeApp/ClientApp/src/components/Button.js
@@ -0,0 +1,11 @@
+﻿import React from 'react'
+
+const Button = ({ showGames, onClick}) => {
+    return (
+        <button className='btn' style={showGames ? { backgroundColor: 'red' } : {backgroundColor : 'green'}} onClick={onClick}>
+            {showGames ? 'Hide my games' : 'Show my games'}
+        </button>
+    )
+}
+
+export default Button
---
+++ GameExchangeApp/ClientApp/src/components/Game.js
@@ -0,0 +1,20 @@
+﻿import React from 'react'
+import Card from 'react-bootstrap/Card'
+
+const Game = ({ title, release, genre }) => {
+    return (
+        <div className='gamecard'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>{ title }</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{ release }</Card.Subtitle>
+                    <Card.Text>
+                        { genre }
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+
+export default Game
