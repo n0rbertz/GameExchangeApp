40fba925d5831dbe642ec53e42ec6928290cd39f
n0rbertz <norbz123@gmail.com>
2022-04-06 18:49:35 +0200

    Added attributes to ApplicationUser

--- GameExchangeApp/Models/ApplicationUser.cs
+++ GameExchangeApp/Models/ApplicationUser.cs
@@ -3,10 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameExchangeApp.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string Location { get; set; }
+        [InverseProperty("OwnedBy")]
+        public virtual ICollection<Game> GamesOwned { get; set; }
+        [InverseProperty("DemandedBy")]
+        public virtual ICollection<Game> GamesDemanded { get; set; }
+        public ApplicationUser()
+        {
+            this.GamesOwned = new HashSet<Game>();
+            this.GamesDemanded = new HashSet<Game>();
+        }
     }
 }
