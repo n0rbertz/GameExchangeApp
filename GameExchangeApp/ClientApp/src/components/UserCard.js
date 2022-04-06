e0302ddc44b789d1ca13f9ed435ecf4206e8a21f
n0rbertz <norbz123@gmail.com>
2022-04-06 16:07:07 +0200

    Created a card for displaying a user

---
+++ GameExchangeApp/ClientApp/src/components/UserCard.js
@@ -0,0 +1,19 @@
+﻿import React from 'react'
+import Game from './Game'
+import Card from 'react-bootstrap/Card'
+
+const User = ({ name, location, games }) => {
+    return (
+        <div className='gamecard'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{location}</Card.Subtitle>
+                    {games.map((game) => (<Game title={game.title} release={game.releaseDate} genre={game.genre} />))}
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+
+export default User
