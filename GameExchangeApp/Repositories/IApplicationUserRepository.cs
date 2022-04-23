2be2c9c08309372004d6b94a30ec754122716a64
n0rbertz <norbz123@gmail.com>
2022-04-23 17:01:32 +0200

    created IApplicationUserRepository

---
+++ GameExchangeApp/Repositories/IApplicationUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using GameExchangeApp.Models;
+
+namespace GameExchangeApp.Repositories
+{
+    public interface IApplicationUserRepository : IDisposable
+    {
+        IEnumerable<ApplicationUser> GetUsers();
+        ApplicationUser GetUserById(int id);
+        void InsertUser(ApplicationUser user);
+        void Save();
+    }
+}
