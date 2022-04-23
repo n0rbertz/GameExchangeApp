4ed33718c3b322a25b813528749f56a855a6bd91
n0rbertz <norbz123@gmail.com>
2022-04-23 15:38:59 +0200

    declared methods in IRepository interface

--- GameExchangeApp/Repositories/IRepository.cs
+++ GameExchangeApp/Repositories/IRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace GameExchangeApp.Repositories
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace GameExchangeApp.Repositories
 {
-    public class IRepository
+    public interface IRepository<T>
     {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+
+        void Remove(int id);
     }
 }
