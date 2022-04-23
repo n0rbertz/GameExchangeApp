0f719ff989589eeb650388b0b683ff4bdca31758
n0rbertz <norbz123@gmail.com>
2022-04-23 22:26:48 +0200

    refactored ApplicationUserController to use ApplicationUserRepository

--- GameExchangeApp/Controllers/ApplicationUserController.cs
+++ GameExchangeApp/Controllers/ApplicationUserController.cs
@@ -8,138 +8,59 @@ using Microsoft.EntityFrameworkCore;
 using GameExchangeApp.Data;
 using GameExchangeApp.Models;
 using GameExchangeApp.DTOs;
+using GameExchangeApp.Repositories;
 
 namespace GameExchangeApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ApplicationUserController : ControllerBase
+    public class UserController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IApplicationUserRepository userRepository;
 
-        public ApplicationUserController(ApplicationDbContext context)
+        public UserController(IApplicationUserRepository userRepository)
         {
-            _context = context;
+            this.userRepository = userRepository;
         }
 
-        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            return await userRepository.GetUsers();
         }
 
         [HttpGet]
         [Route("{id}/gamesowned")]
         public async Task<ActionResult<IEnumerable<Game>>> GetOwnedGamesOfUser(string id)
         {
-            var user = await _context.Users.Include("GamesOwned").FirstOrDefaultAsync(u => u.Id.Equals(id));
-            return user.GamesOwned.ToList();
-        }
-
-        [HttpGet]
-        [Route("{id}/matches")]
-        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetMatches(string id)
-        {
-            var user = await _context.Users.Include("GamesOwned").Include("GamesDemanded").FirstOrDefaultAsync(u => u.Id.Equals(id));
-            var usersWithSameLocation = _context.Users.Include("GamesOwned").Include("GamesDemanded").Where(u => u.Location == user.Location);
-            var gamesDemanded = user.GamesDemanded;
-            var usersWithDemandedGame = new List<ApplicationUser>();
-            foreach (Game game in gamesDemanded)
-            {
-                foreach(ApplicationUser u in usersWithSameLocation)
-                {
-                    if (u.GamesOwned.Contains(game))
-                    {
-                        usersWithDemandedGame.Add(u);
-                    }
-                }
-            }
-            var matches = new List<ApplicationUser>();
-            foreach (ApplicationUser u in usersWithDemandedGame)
-            {
-                foreach (Game game in u.GamesDemanded)
-                {
-                    if (u.GamesOwned.Contains(game))
-                    {
-                            matches.Add(u);
-                    }
-                }
-            }
-            return matches.ToList();
+            return await userRepository.GetOwnedGamesOfUser(id);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<GamerDTO>> GetUser(string id)
+        public async Task<ActionResult<ApplicationUser>> GetUser(string id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var user = await userRepository.GetUserById(id);
 
             if (user == null)
             {
                 return NotFound();
             }
 
-            return new GamerDTO { Name = user.UserName, Location = user.Location };
-        }
-
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(string id, ApplicationUser user)
-        {
-            if (id != user.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            return user;
         }
 
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Gamer>> PostUser([FromBody] ApplicationUser user)
+        public ActionResult<ApplicationUser> PostUser([FromBody] ApplicationUser user)
         {
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            userRepository.InsertUser(user);
+            userRepository.Save();
 
-            return CreatedAtAction("GetGamer", new { id = user.Id }, user);
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(string id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+        
 
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
-
-        private bool UserExists(string id)
-        {
-            return _context.Users.Any(e => e.Id == id);
-        }
     }
 }
