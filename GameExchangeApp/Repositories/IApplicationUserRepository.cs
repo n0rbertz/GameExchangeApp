7940ead17f9e0fc8a3692fae8a55e287dadd6152
n0rbertz <norbz123@gmail.com>
2022-04-23 22:24:10 +0200

    declare methods in IApplicationUserRepository

--- GameExchangeApp/Repositories/IApplicationUserRepository.cs
+++ GameExchangeApp/Repositories/IApplicationUserRepository.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameExchangeApp.DTOs;
 using GameExchangeApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameExchangeApp.Repositories
 {
-    public interface IApplicationUserRepository : IDisposable
+    public interface IApplicationUserRepository 
     {
-        IEnumerable<ApplicationUser> GetUsers();
-        ApplicationUser GetUserById(int id);
-        void InsertUser(ApplicationUser user);
+        Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers();
+        Task<ActionResult<ApplicationUser>> GetApplicationUserById(string id);
+        Task<ActionResult<ApplicationUser>> GetUserWithOwnedAndDemandedGames(string id);
+        Task<ActionResult<IEnumerable<Game>>> GetOwnedGamesOfUser(string id);
+        void InsertApplicationUser(ApplicationUser applicationUser);
         void Save();
     }
 }
