53bfee8cc17576a92b7ff43161c7f3db5c668421
n0rbertz <norbz123@gmail.com>
2022-04-26 16:46:16 +0200

    Implemented IRepository methods in Repository

---
+++ GameExchangeApp/Repositories/Repository.cs
@@ -0,0 +1,40 @@
+﻿using GameExchangeApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GameExchangeApp.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        internal ApplicationDbContext context;
+        internal DbSet<TEntity> dbset;
+
+        public Repository(ApplicationDbContext context)
+        {
+            this.context = context;
+            this.dbset = context.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public async Task<ActionResult<IEnumerable<TEntity>>> GetAll()
+        {
+            return await dbset.ToListAsync();
+        }
+
+        public async Task<ActionResult<TEntity>> GetById(string id)
+        {
+            return await dbset.FindAsync(id);
+        }
+
+        public void Save()
+        {
+            context.SaveChangesAsync();
+        }
+    }
+}
