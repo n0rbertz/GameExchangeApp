984bec43af47af602d43d6d3cbaf7ba0cd10ba1b
n0rbertz <norbz123@gmail.com>
2022-04-26 16:45:44 +0200

    fixed abstract method signatures and return types in IRepository

--- GameExchangeApp/Repositories/IRepository.cs
+++ GameExchangeApp/Repositories/IRepository.cs
@@ -1,17 +1,17 @@
-﻿using System;
+﻿using GameExchangeApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace GameExchangeApp.Repositories
 {
-    public interface IRepository<T>
+    public interface IRepository<TEntity> where TEntity : class
     {
-        T Get(int id);
-        IEnumerable<T> GetAll();
-        
-        void Add(T entity);
-        void AddRange(IEnumerable<T> entities);
-
-        void Remove(int id);
+        Task<ActionResult<TEntity>> GetById(string id);
+        Task<ActionResult<IEnumerable<TEntity>>> GetAll();        
+        void Add(TEntity entity);
+        public void Save();
     }
 }
