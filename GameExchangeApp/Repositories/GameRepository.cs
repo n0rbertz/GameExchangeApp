50fd62578b4ed624e6b1972aba55c56edbd1736d
n0rbertz <norbz123@gmail.com>
2022-10-10 18:47:27 +0200

    create GameRepository class and interface

---
+++ GameExchangeApp/Repositories/GameRepository.cs
@@ -0,0 +1,23 @@
+﻿using GameExchangeApp.Data;
+using GameExchangeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GameExchangeApp.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        private readonly ApplicationDbContext context;
+        public async Task<ActionResult<Game>> GetGameById(int id)
+        {
+            return await context.Games.FindAsync(id);
+        }
+
+        public async Task<ActionResult<IEnumerable<Game>>> GetGames()
+        {
+            return await context.Games.ToListAsync();
+        }
+    }
+}
---
+++ GameExchangeApp/Repositories/IGameRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameExchangeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameExchangeApp.Repositories
+{
+    public interface IGameRepository
+    {
+
+        Task<ActionResult<IEnumerable<Game>>> GetGames();
+        Task<ActionResult<Game>> GetGameById(int id);
+
+    }
+}
